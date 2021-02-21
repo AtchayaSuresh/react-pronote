1b4f65172206c4a7623f611e17a38d4cef022efd
Chams <atchayasuresh17@gmail.com>
2021-02-22 03:07:19 +0530

    '1721994'

---
+++ pronote/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Editor from './components/Editor';
+import './App.css';
+import Editor from './components/Editor';
+
+function App() {
+  return (
+    <div className="App">
+      <Editor/>
+    </div>
+  );
+}
+
+export default App;
